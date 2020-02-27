9fb9aef85a1b27b4df1b5190af00e6444898004f
Euan Lau <euan@catchandrelease.com>
2020-02-27 14:36:23 -0800

    Fix auth hash not returning long lived token

--- lib/omniauth/strategies/instagram-graph.rb
+++ lib/omniauth/strategies/instagram-graph.rb
@@ -71,32 +71,16 @@ module OmniAuth
         end
       end
 
-      def callback_phase
-        error = request.params['error_reason'] || request.params['error']
-        if error
-          fail!(error,
-            CallbackError.new(
-              request.params['error'],
-              request.params['error_description'] || request.params['error_reason'],
-              request.params['error_uri']
-            ))
-        elsif !options.provider_ignores_state && (request.params['state'].to_s.empty? || request.params['state'] != session.delete('omniauth.state'))
-          fail!(:csrf_detected, CallbackError.new(:csrf_detected, 'CSRF detected'))
-        else
-          self.access_token = build_access_token
+      protected
 
-          if options.exchange_access_token_to_long_lived
-            self.access_token = long_lived_client.get_token(access_token: access_token.token)
-          end
+      def build_access_token
+        access_token = super
 
-          super
+        if options.exchange_access_token_to_long_lived
+          access_token = long_lived_client.get_token(access_token: access_token.token)
         end
-      rescue ::OAuth2::Error, CallbackError => e
-        fail!(:invalid_credentials, e)
-      rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
-        fail!(:timeout, e)
-      rescue ::SocketError => e
-        fail!(:failed_to_connect, e)
+
+        access_token
       end
 
       private
